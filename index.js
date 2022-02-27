262c445f30dc28ccec124a0392f04cf60b964644
Fry-Fr <ryjlwdg89@gmail.com>
2022-02-26 19:25:33 -0500

    base socket server

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { Server } = require('socket.io');
+
+const clients = require('./clients');
+
+const io = new Server({cors: '*'});
+
+io.on("connection", (socket) => {
+
+    socket.on("setClient", (usrObj) => {
+        socket.name = usrObj.name;
+        clients[usrObj.name] = usrObj.letter;
+        io.emit("clientList", clients)
+        console.log('connected', clients)
+    })
+    
+    socket.on("disconnect", () => {
+        delete clients[socket.name]
+        io.emit("clientList", clients)
+        console.log('user disconnected', socket.name)
+    })
+})
+
+io.listen(process.env.SOCKETPORT || 3333);
+console.log(`**listening on port ${process.env.PORT}`)
